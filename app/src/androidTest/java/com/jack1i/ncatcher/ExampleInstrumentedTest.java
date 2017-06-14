f7c655c3104304a023fa2f65726ab3cf115f4e33
jackdev <lijack.dev@gmail.com>
2017-06-15 00:46:16 +0800

    first commit

---
+++ app/src/androidTest/java/com/jack1i/ncatcher/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.jack1i.ncatcher;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.jack1i.ncatcher", appContext.getPackageName());
+    }
+}
